f5ef6ba3db2e292d6017651aa2b93ff4f871da42
cmh <834719562@qq.com>
2024-02-11 12:09:47 +0800

    sync

--- docker/gen_image.sh
+++ docker/gen_image.sh
@@ -3,4 +3,4 @@ cd ..
 docker rmi rss_crawler:slim
 docker build . -t rss_crawler:slim
 #docker save rss_crawler:slim -o rss_crawler_slim.tar
-docker run --rm -d --name rss_crawler -v ./sqlite3.sql:/code/sqlite3.sql rss_crawler:slim
+docker run --rm -d --name rss_crawler -v ./sqlite3.sql:/code/sqlite3.sql -v ./log:/code/log rss_crawler:slim
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -d --name rss_crawler -v ./sqlite3.sql:/code/sqlite3.sql -v ./config:/code/config rss_crawler:slim
+docker run --rm -d --name rss_crawler -v ./sqlite3.sql:/code/sqlite3.sql -v ./log:/code/log rss_crawler:slim
