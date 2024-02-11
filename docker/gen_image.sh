038794f8ff88d6b931c8586e163e8c5733b82efe
vps <vps>
2024-02-11 02:56:55 +0000

    update

--- docker/gen_image.sh
+++ docker/gen_image.sh
@@ -2,4 +2,5 @@
 cd ..
 docker rmi rss_crawler:slim
 docker build . -t rss_crawler:slim
-docker save rss_crawler:slim -o rss_crawler_slim.tar
+#docker save rss_crawler:slim -o rss_crawler_slim.tar
+docker run --rm -d --name rss_crawler -v ./sqlite3.sql:/code/sqlite3.sql rss_crawler:slim
