37343563e3634d7df73b3205905c99d4c469dd3d
vps <vps>
2024-02-18 05:15:51 +0000

    sync

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build . -t rss_crawler
-docker run --name rss_crawler -v ./sqlite3.sql:/code/sqlite3.sql -v ./RssParser:/code/RssParser -v ./log:/code/log rss_crawler
+docker run --name rss_crawler -d -v ./sqlite3.sql:/code/sqlite3.sql -v ./RssParser:/code/RssParser -v ./log:/code/log rss_crawler
