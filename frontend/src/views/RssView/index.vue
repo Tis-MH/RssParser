b7ee983596e056ef7f01ea21227ff9a834a44882
cm <you@example.com>
2024-03-21 01:26:21 +0800

    first

---
+++ frontend/src/views/RssView/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="border-gray border-3 rounded-3 border-solid
+        m-3 p-3">
+            something here
+        </div>
+        <div>
+            <p class="font-sans">AI will ruling the world</p>
+            <p>i trust, so, at that time, first is make a model to </p>
+            <p>extract all message and ...</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
