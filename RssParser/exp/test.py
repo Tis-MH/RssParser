190f840974da30d2d9525e9dc8a5931ebbcfb434
cmh <834719562@qq.com>
2024-04-09 23:14:25 +0800

    sync

---
+++ RssParser/exp/test.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    DateTime,
+    create_engine,
+    Boolean,
+)
+import hashlib
+import requests
+from os import path
+from loguru import logger
+
+
+logger.add("log/file_2.log", rotation="10 MB", level="INFO")
+
+from sqlalchemy.orm import declarative_base, Session, relationship
+
+engine = create_engine("sqlite:///sqlite.sql")
+
+session = Session(engine)
+
+Base = declarative_base()
+
+
+class SubscribeWebsite(Base):
+    __tablename__ = "subscribe_website"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    url = Column(String(255), nullable=False, unique=True)
+    enable = Column(Boolean(), nullable=False)
+
+
+class SubscribeRecord(Base):
+    __tablename__ = "subscribe_record"
+    id = Column(String(255), primary_key=True)
+    update_time = Column(DateTime, nullable=False)
+    website_id = Column(Integer, ForeignKey("subscribe_website.id"), nullable=False)
+
+    website = relationship(
+        "SubscribeWebsite", foreign_keys=[website_id], backref="records"
+    )
+
+
+def download_rss(url: str, save_path="./result", max_retries=3):
+    retries = 0
+
+    def request(url: str):
+        nonlocal retries
+        try:
+            response = requests.get(url, timeout=10)
+            logger.info(f"success download {url}")
+            return response
+        except requests.Timeout:
+            retries += 1
+            logger.warning(f"retry to connect: {url}... {retries}/{max_retries}")
+            if retries > max_retries:
+                logger.error(f"connect to {url} timeout, retries: {max_retries}")
+                raise requests.Timeout
+            return request(url)
+
+    html = request(url).text
+    file_hash = hashlib.sha1(html.encode("utf-8")).hexdigest()
+    with open(path.join(save_path, file_hash + ".xml"), "w", encoding="utf-8") as f:
+        f.write(html)
+    return file_hash
+
+
+def download_all():
+    websites = session.query(SubscribeWebsite).where(SubscribeWebsite.enable == True)
+    for website in websites:
+        file_hash = download_rss(website.url)
+        record = SubscribeRecord(
+            id=file_hash, update_time=datetime.now(), website_id=website.id
+        )
+        session.add(record)
+        session.commit()
+
+
+def add_subscribe():
+    subscibes = [SubscribeWebsite(
+        name="dmhy", url="https://dmhy.org/topics/rss/rss.xml", enable=True
+    ),
+    SubscribeWebsite(
+        name="mori", url="https://share.acgnx.se/rss.xml", enable=True
+    )]
+    session.add_all(subscibes)
+    session.commit()
+
+
+Base.metadata.create_all(engine)
+# session.add(website)
+# session.commit()
+
+add_subscribe()
+# download_all()
+# download_rss("https://dmhy.org/topics/rss/rss.xml")
--- dependancy/ollama_get.py
+++ dependancy/ollama_get.py
@@ -2,44 +2,55 @@ import requests
 import json
 import re
 
+
+def get_entity():
+    import sqlite3
+
+    file_path = "sqlite3.sql"
+    db = sqlite3.connect(file_path)
+    cursor = db.cursor()
+    res = cursor.execute("select * from entity")
+    for i in res.fetchall():
+        yield i[0]
+
+
 def info_extract(title, retires=3):
     def question(title):
         content = requests.post(
-            "http://localhost:11434/api/chat",
+            "http://localhost:11434/api/generate",
+            # json={
+            #     "model": "codellama:7b",
+            #     "messages": [
+            #         {
+            #             "role": "user",
+            #             "content": title
+            #             + " 按格式 {字幕组: '', 标题: '', 清晰度: '', 集数: ''} 提取这条信息中的字幕组, 标题, 清晰度和集数",
+            #         },
+            #         # {"role": "assistant", "content": "due to rayleigh scattering."},
+            #         # {"role": "user", "content": "how is that different than mie scattering?"},
+            #     ],
+            #     "stream": True,
+            # },
             json={
-                "model": "yi",
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": title
-                        + " 提取这条信息中的字幕组, 标题, 清晰度和集数, 按格式 {字幕组: '', 标题: '', 清晰度: '', 集数: ''} 返回 ",
-                    },
-                    # {"role": "assistant", "content": "due to rayleigh scattering."},
-                    # {"role": "user", "content": "how is that different than mie scattering?"},
-                ],
-                "stream": True,
+                "model": "codellama:7b",
+                "prompt": title
+                + " 按格式 {字幕组: '', 标题: '', 清晰度: '', 集数: ''} 提取这条信息中的字幕组, 标题, 清晰度和集数",
+                "stream": False,
             },
         )
 
-        res = ""
-        lines = content.text.split("\n")
-        for i in lines[:-1]:
-            print(i)
-            res += json.loads(i)["message"]["content"]
-        return res
+        print(content.json()["response"])
 
-    for _ in range(retires):
-        answer = question(title)
-        try:
-            return re.search("{.*?}", answer)
-        except Exception as e:
-            print(e)
+    question(title)
 
-    raise TimeoutError
 
 if __name__ == "__main__":
-    print(
-        info_extract(
-            "【喵萌奶茶屋】★01月新番★[反派千金Lv99 雖然我是隱藏BOSS但我可不是魔王 / Akuyaku Reijou Level 99][03][1080p][繁日雙語][招募翻譯]"
-        )
-    )
+    title = "	[jibaketa合成&音頻壓制][TVB粵語]閃躍吧！星夢☆頻道 / 美妙☆频道 / Kiratto Pri-chan - 98 [粵日雙語+內封繁體中文字幕][WEB 1920x1080 x264 AACx2 SRT TVB CHT]"  # "【喵萌奶茶屋】★01月新番★[反派千金Lv99 雖然我是隱藏BOSS但我可不是魔王 / Akuyaku Reijou Level 99][03][1080p][繁日雙語][招募翻譯]"
+    info_extract(title)
+    # for title in get_entity():
+    #     print(title)
+    #     print(
+    #         info_extract(
+    #             title
+    #         )
+    #     )
