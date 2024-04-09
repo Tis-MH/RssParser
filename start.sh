c33c8e7c8fe1aea672a267587c5d1cf28f19765d
vps <vps>
2024-04-09 14:06:31 +0100

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
+docker rm rss_crawler
 #docker build . -t rss_crawler
-git pull
 docker run --name rss_crawler -d -v ./sqlite3.sql:/code/sqlite3.sql -v ./RssParser:/code/RssParser -v ./log:/code/log rss_crawler
